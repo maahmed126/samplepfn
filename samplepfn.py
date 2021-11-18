e5b63f33f4c28ea119f1682b67319bc59fd4451c
Md Ahmed <maahmed126@gmail.com>
2021-11-18 13:57:40 +0530

    Update samplepfn.py

--- samplepfn.py
+++ samplepfn.py
@@ -1,6 +1,6 @@
 def handler(event,context):
 
-    kg=event['queryStringParameters']['kg']
+    kg=event['kg']
     lb=float(kg)*2.20462
     ans = str(kg) + " Kgs is " + str(round(lb, 2)) + " lbs\n"
 
